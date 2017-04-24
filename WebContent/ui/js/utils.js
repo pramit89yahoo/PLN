ff67226df9eaeb4ffd56936f766892e07cbdcd51
Pramit <parikh.pramit89@gmail.com>
2017-04-24 12:57:06 +0530

    Previous changes commit.

--- WebContent/ui/js/utils.js
+++ WebContent/ui/js/utils.js
@@ -9,3 +9,26 @@ utils.validateEmail=function(email) {
 		return true;
 	}
 };
+utils.fillComboCustom = function(comboName, data, value, text, firstElmValue, firstElmText) {
+    var allVal = new Array();
+    var combo = document.getElementById(comboName);
+    combo.innerHTML = "";
+
+    if (firstElmValue != null && firstElmText != null) {
+        var option = document.createElement('option');
+        option.value = firstElmValue;
+        option.text = firstElmText;
+        combo.add(option, combo.options[null]);
+    }
+
+    var gList = data.data;
+    var gListLen = gList.length;
+    for (var i = 0; i < gListLen; i++) {
+        var option = document.createElement('option');
+        allVal.push(gList[i][value]);
+        option.value = gList[i][value];
+        option.text = gList[i][text];
+        combo.add(option, combo.options[null]);
+    }
+    return allVal;
+};
